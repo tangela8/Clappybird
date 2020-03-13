a7c33a0b297dc8d92b468f684c4f120f87fac0f1
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-03-13 14:30:25 -0400

    updated buttons in js

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ let myScore = 0;
 const gameCanvas = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 600;//drawing the game area
+        this.canvas.width = 800;//drawing the game area
         this.canvas.height = 500;//drawing the game area
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);//setting the speed of the pipes
+        //setting the speed of the pipes
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener("keydown", function (moveUp) {
+            gameCanvas.key = moveUp.keyCode;
+        })
+        window.addEventListener("keyup", function (moveDown) {
+            gameCanvas.key = false;
+        })
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -60,13 +67,23 @@ function bird(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY //+ this.gravitySpeed;
         this.hitBottom();
+        //this.hitTop();
     }
     //stop the falling when it hits the bottom of the game area
     this.hitBottom = function() {
-        var rockbottom = gameCanvas.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
-            this.gravitySpeed = 0;
+        let rockBottom = gameCanvas.canvas.height - this.height;
+        if (this.y > rockBottom) {
+            this.y = rockBottom;
+            //this.gravitySpeed = 0;
+        }
+    }
+    this.hitTop = function() {
+        let rockTop = gameCanvas.canvas.height - this.height;
+        // if (this.y < rockTop) {
+        //     this.y = rockTop;
+        //     this.height = 0;
+        if (this.y - this.height < 0) {
+            this.y = this.height;
         }
     }
     this.crashWith = function(obstacles) {
@@ -88,42 +105,32 @@ function bird(width, height, color, x, y, type) {
 
 //game controls
 
-// function up(n) {
-//     myGamePiece.gravity = n;
+// function clearMove() {
+//     myGamePiece.speedX = 0; 
+//     myGamePiece.speedY = 0; 
 // }
-function moveUp() {
-    myGamePiece.speedY = -2; 
-    
-}
-
-function moveDown() {
-    myGamePiece.speedY = 2; 
-}
-
-function clearMove() {
-    myGamePiece.speedX = 0; 
-    myGamePiece.speedY = 0; 
-}
 //setting the distance between the pipes
-function everyinterval(n) {
+function everyInterval(n) {
     if ((gameCanvas.frameNo / n) % 1 == 0) {return true;}
     return false;
 }
 
 //putting movements and obstacles into game area
 function updateGameArea() {
-    myGamePiece.newPos();
-    myGamePiece.update();
     let x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < pipes.length; i += 1) {
         if (myGamePiece.crashWith(pipes[i])) {
             return;
         } 
     }
-    gameCanvas.clear();
-    gameCanvas.frameNo += 1;
+    gameCanvas.clear();//must clear from last frame, if left out will leave traces
+    myGamePiece.speedY = 0;
+    if (gameCanvas.key && gameCanvas.key == 38) {myGamePiece.speedY = -1;}
+    //if (gameCanvas.key && gameCanvas.key == 39) {myGamePiece.speedX = 1;}
+    if (gameCanvas.key && gameCanvas.key == 40) {myGamePiece.speedY = 1;}
+    gameCanvas.frameNo += 1;//making it move
     //conditions for the pipes
-    if (gameCanvas.frameNo == 1 || everyinterval(200)) {
+    if (gameCanvas.frameNo == 1 || everyInterval(200)) {
         x = gameCanvas.canvas.width;
         minHeight = 20;
         maxHeight = 200;
@@ -135,7 +142,7 @@ function updateGameArea() {
         pipes.push(new bird(30, x - height - gap, "green", x, height + gap));
     }
     for (i = 0; i < pipes.length; i += 1) {
-        pipes[i].x += -1;
+        pipes[i].x += -1;//making the pipes move
         pipes[i].update();
     }
     myScore.text="SCORE: " + gameCanvas.frameNo;
